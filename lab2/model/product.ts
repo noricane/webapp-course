7ef00c6aee9a1959a86cb61bd2abb49c60d02434
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-02 22:44:47 +0100

    Working on product class rn

--- lab2/model/product.ts
+++ lab2/model/product.ts
@@ -7,7 +7,16 @@ export class Product{
     in_stock:boolean;
     price_factor:number; //if factor < 1 then product is on sale
 
-    constructor(name:string,description:string,price:number,){}
+    constructor(name:string,description:string,price:number,category:string,in_stock:boolean,price_factor:number){
+        this.id = Date.now()
+        this.name =name;
+        this.description =description;
+        this.price = price;
+        this.category =category;
+        this.in_stock = in_stock;
+        this.price_factor = price_factor;
+
+    }
 
 
 
