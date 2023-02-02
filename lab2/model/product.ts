81c1ff88ac61b10f58797451e0796a745de74b59
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-02 22:39:36 +0100

    added modules

---
+++ lab2/model/product.ts
@@ -0,0 +1,14 @@
+export class Product{
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    category:string;
+    in_stock:boolean;
+    price_factor:number; //if factor < 1 then product is on sale
+
+    constructor(name:string,description:string,price:number,){}
+
+
+
+}
