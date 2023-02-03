282abc57e495138ccc031997b67050b5347ef3b8
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-03 09:44:50 +0100

    added images to product class

--- lab2/model/product.ts
+++ lab2/model/product.ts
@@ -6,8 +6,9 @@ export class Product{
     category:string;
     in_stock:boolean;
     price_factor:number; //if factor < 1 then product is on sale
+    images: string[];//array of urls.
 
-    constructor(name:string,description:string,price:number,category:string,in_stock:boolean,price_factor:number){
+    constructor(name:string,description:string,price:number,category:string,in_stock:boolean,price_factor:number, ...url:string[]){
         this.id = Date.now()
         this.name =name;
         this.description =description;
@@ -15,7 +16,8 @@ export class Product{
         this.category =category;
         this.in_stock = in_stock;
         this.price_factor = price_factor;
-
+        this.images = [];
+        url.forEach(e => this.images.push(e))
     }
 
 
