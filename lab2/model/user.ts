63a6184d7b5875926b1fa24356133d696492ef9b
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-06 17:45:26 +0100

    added another constructor :)

--- lab2/model/user.ts
+++ lab2/model/user.ts
@@ -8,4 +8,12 @@ export class user {
     birthdate: string;
     adresses: address[];
 
+    constructor(id: number,name: string,email: string,phonenumber: string,birthdate: string,adresses: address[]){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phonenumber = phonenumber;
+        this.birthdate = birthdate;
+        this.adresses = adresses;
+    }
 }
