16f4b743f6523bf2afdab7c2a25b0c299d3c7c2c
noricane <55191187+noricane@users.noreply.github.com>
2023-02-17 20:19:31 +0100

    loggin off

--- lab3/src/components/Nav.tsx
+++ lab3/src/components/Nav.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Heeeeh</div>
+    <div className='w-screen bg-stone-300 h-24 flex text-green-900 font-bold'>
+      <div className='font-3xl'>logo</div>
+      <div className='flex'>
+
+      <ul id="navLinks" className='flex'>
+        <li>Home</li>
+        <li>Categories</li>
+        <li>What's new?</li>
+        <li>Member's club</li>
+        <li className='mx-4 bg-blue-400 rounded-lg border-emerald-400 border-4'>Lög in</li>
+      </ul>
+      </div>
+    </div>
   )
 }
 
