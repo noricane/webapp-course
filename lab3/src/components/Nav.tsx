e8025816c55ad01d74dff06b0151ab592decbffc
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-18 16:45:00 +0100

    nav looking crispy

--- lab3/src/components/Nav.tsx
+++ lab3/src/components/Nav.tsx
@@ -1,23 +1,31 @@
-import React, { DetailedHTMLProps, EventHandler, VideoHTMLAttributes } from 'react'
-
+import { useState } from 'react'
+import {Twirl as Hamburger} from 'hamburger-react'
 const Nav = () => {
+  const [isOpen, setOpen] = useState(false)
   const homeButtonHandler = (e:any) => {
     e?.preventDefault();
     //router push home
   }
   return (
-    <div className='w-screen bg-stone-300  h-24 items-center flex text-zinc-800 font-bold'>
-      <video onClick={homeButtonHandler} src={'media/logo.mp4'} autoPlay loop className='hover:cursor-pointer h-24 left-0 video mix-blend-darken'/>
-      <div className='flex justify-center h-24 items-center absolute w-screen '>
-
-      <ul id="navLinks" className='flex gap-8 mx-auto'>
+    <div className='w-screen  bg-stone-200  h-24 items-center flex text-zinc-800 font-bold'>
+      <video onClick={homeButtonHandler} src={'media/logo.mp4'} muted playsInline autoPlay loop className='hover:cursor-pointer h-24 utmd:mx-auto md:left-0 video mix-blend-darken'/>
+      <div className='flex justify-center  h-24 items-center absolute w-screen '>
+      <div className='left-0 mx-4 md:hidden absolute'><Hamburger toggled={isOpen} toggle={setOpen} /></div>
+      <ul id="navLinks" className='flex utmd:hidden gap-8 mx-auto'>
         <li>Home</li>
         <li>Categories</li>
         <li>What's new?</li>
         <li>Member's club</li>
+        <div className='xs:hidden'><span className='hover:bg-black hover:text-stone-200  bg-stone-200 p-1 border-black border-4'>Sign in</span>
+        <span className='bg-black text-stone-200  hover:bg-stone-200 hover:text-black p-1 border-black border-4'>Sign in</span>
+        </div>
       </ul>
       </div>
-        <span className='absolute right-0 mx-4 bg-blue-400 rounded-lg border-emerald-400 border-4'>Lög in</span>
+        <div className='absolute utxs:hidden right-0 mx-4 flex gap-2'>
+        <span className='hover:bg-black hover:text-stone-200  bg-stone-200 p-1 border-black border-4'>Sign in</span>
+        <span className='bg-black text-stone-200  hover:bg-stone-200 hover:text-black p-1 border-black border-4'>Sign in</span>
+        
+        </div>
     </div>
   )
 }
--- lab3/tailwind.config.js
+++ lab3/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'utmd': {'max':'767px'},
+        'utxs': {'max':'480px'},
+        'xs': {'min':'480px'},
+      }
+    },
   },
   plugins: [],
 }
