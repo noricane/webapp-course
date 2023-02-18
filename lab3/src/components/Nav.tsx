6ca61c94a7031c0628e0dce87671b9b6de21db4c
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-18 16:17:39 +0100

    working on nav

--- lab3/src/components/Nav.tsx
+++ lab3/src/components/Nav.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { DetailedHTMLProps, EventHandler, VideoHTMLAttributes } from 'react'
 
 const Nav = () => {
+  const homeButtonHandler = (e:any) => {
+    e?.preventDefault();
+    //router push home
+  }
   return (
-    <div className='w-screen bg-stone-300 h-24 flex text-green-900 font-bold'>
-      <div className='font-3xl'>logo</div>
-      <div className='flex'>
+    <div className='w-screen bg-stone-300  h-24 items-center flex text-zinc-800 font-bold'>
+      <video onClick={homeButtonHandler} src={'media/logo.mp4'} autoPlay loop className='hover:cursor-pointer h-24 left-0 video mix-blend-darken'/>
+      <div className='flex justify-center h-24 items-center absolute w-screen '>
 
-      <ul id="navLinks" className='flex'>
+      <ul id="navLinks" className='flex gap-8 mx-auto'>
         <li>Home</li>
         <li>Categories</li>
         <li>What's new?</li>
         <li>Member's club</li>
-        <li className='mx-4 bg-blue-400 rounded-lg border-emerald-400 border-4'>Lög in</li>
       </ul>
       </div>
+        <span className='absolute right-0 mx-4 bg-blue-400 rounded-lg border-emerald-400 border-4'>Lög in</span>
     </div>
   )
 }
