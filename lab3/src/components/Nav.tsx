52c8eed2e4aa32710184e815cf9445edfeaac3dc
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-18 16:58:24 +0100

    nav looking crispy

--- lab3/src/components/Nav.tsx
+++ lab3/src/components/Nav.tsx
@@ -8,7 +8,7 @@ const Nav = () => {
   }
   return (
     <div className='w-screen  bg-stone-200  h-24 items-center flex text-zinc-800 font-bold'>
-      <video onClick={homeButtonHandler} src={'media/logo.mp4'} muted playsInline autoPlay loop className='hover:cursor-pointer h-24 utmd:mx-auto md:left-0 video mix-blend-darken'/>
+      <video onClick={homeButtonHandler} src={'media/logo.mp4'} muted playsInline draggable="false" autoPlay loop className='hover:cursor-pointer  h-24 utmd:mx-auto md:left-0 video mix-blend-darken'/>
       <div className='flex justify-center  h-24 items-center absolute w-screen '>
       <div className='left-0 mx-4 md:hidden absolute'><Hamburger toggled={isOpen} toggle={setOpen} /></div>
       <ul id="navLinks" className='flex utmd:hidden gap-8 mx-auto'>
@@ -21,7 +21,7 @@ const Nav = () => {
         </div>
       </ul>
       </div>
-        <div className='absolute utxs:hidden right-0 mx-4 flex gap-2'>
+        <div className='absolute utxs:hidden [&>*]:hover:cursor-pointer right-0 mx-4 flex gap-2'>
         <span className='hover:bg-black hover:text-stone-200  bg-stone-200 p-1 border-black border-4'>Sign in</span>
         <span className='bg-black text-stone-200  hover:bg-stone-200 hover:text-black p-1 border-black border-4'>Sign in</span>
         
