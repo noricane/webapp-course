7459677d77151795a164f78d3f3c8aaa20ad93de
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-02-28 21:07:21 +0100

    kjhkhj

--- lab3/src/components/Layout/Footer.tsx
+++ lab3/src/components/Layout/Footer.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full h-96 bg-stone-800'></div>
+    <div className='w-full h-96 bg-stone-800'>
+      <section>
+        <h1 className='font-oswald'>A B O U T</h1>
+        <ul>
+          <li>About us</li>
+          <li>Member's Club</li>
+          <li></li>
+        </ul>
+      </section>
+      <section>
+
+      </section>
+      <section>
+        <h1>N E W S L E T T E R</h1>
+      </section>
+    </div>
   )
 }
 
