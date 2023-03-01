b1703220f3f73fe7f9f6f3ffab21622a540ec7c0
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-03-01 20:02:09 +0100

    updated dummy product

--- lab2/src/service/dummyproducts.ts
+++ lab2/src/service/dummyproducts.ts
@@ -1,13 +1,14 @@
+import { GENERALCOLOR } from './../helper/utils';
 import { normalizeString, productConstructor } from '../helper/utils';
 import {Product} from '../model/product'
 
 export function initShoes():Map<string,Map<string,Product>>{
     const map: Map<string,Map<string,Product>> = new Map();
     
-    const p1 = createProduct("Air Jordan 1 low","Nike","Royal Blue",1249);
-    const p2 = createProduct("Air Jordan 1 low","Nike","Celtic Green",1249);
-    const p3 = createProduct("Foam Runners","Yeezy","Sand",3495);
-    const p4 = createProduct("Foam Runners","Yeezy","MXT Moon Grey",3495);
+    const p1 = createProduct("Air Jordan 1 low","Nike","Royal Blue",GENERALCOLOR.BLUE,1249);
+    const p2 = createProduct("Air Jordan 1 low","Nike","Celtic Green",GENERALCOLOR.GREEN,1249);
+    const p3 = createProduct("Foam Runners","Yeezy","Sand",GENERALCOLOR.BEIGE,3495);
+    const p4 = createProduct("Foam Runners","Yeezy","MXT Moon Grey",GENERALCOLOR.GRAY,3495);
     
     const lp = [p1,p2,p3,p4]
 
@@ -36,13 +37,13 @@ export function initShoes():Map<string,Map<string,Product>>{
 }
 
 
-const createProduct = (name:string, brand: string, color: string, price: number):Product=>{
+const createProduct = (name:string, brand: string, color: string,generalColor:GENERALCOLOR, price: number):Product=>{
     const luh =  [
         {
             size: 42,
             amount: 42
         }
     ]
-    return new Product(name,brand,"Shoes",color,price,"Low Sneakers",luh,1,[`https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80`])
+    return new Product(name,brand,"Shoes",color,generalColor,price,"Low Sneakers",luh,1,[`https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80`])
 
 }
