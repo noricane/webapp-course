169840926ca440d8ed7875e25403475d62f40a80
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2023-03-10 19:45:00 +0100

    cleaning up routers

--- lab3/website/src/pages/Cart.tsx
+++ lab3/website/src/pages/Cart.tsx
@@ -4,6 +4,13 @@ import { cartAtom } from '../model/jotai.config'
 
 const Cart = () => {
     const [cart,setCart] = useAtom(cartAtom)
+    if(cart.length == 0){
+        return <div className='min-h-[70vh] p-8 bg-stone-300 m-12 rounded-xl flex flex-col justify-center items-center font-oswald text-3xl'>
+            Cart is empty
+            <span className='text-lg text-stone-700 text-center'>Consider adding products before checking cart</span>
+        </div>
+    
+    }
   return (
     <div className='min-h-[70vh] p-8 bg-stone-300 m-12 rounded-xl'>{cart.map(e => <div>{e.amount}</div>)}</div>
   )
