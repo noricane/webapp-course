1a6b2cbb2abca349c8635661548d0e5e7ac562d3
Tarek Chorfi <55191187+noricane@users.noreply.github.com>
2024-03-24 15:02:48 +0100

    cors=*

--- lab2/src/start.ts
+++ lab2/src/start.ts
@@ -31,11 +31,11 @@ secret : process.env.SESSION_KEY as string || "",
 resave : false, //Whether or not state is saved everytime? or whenever session is modified
 saveUninitialized : true //Decides wherther a blank session is saved or if it waits until the user writes something to the session object
 })); */
-/* app.use(cors({
-origin: true, //Allows requests from any origin, not good for production
+app.use(cors({
+origin: '*', //Allows requests from any origin, not good for production
 methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD','DELETE'],
 credentials : true //Specifies if express should include session id in every response, if this is true then origin property must be specified. 
-})); */
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/product", product_router);
